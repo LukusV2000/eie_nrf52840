435500eff6c83c050eb6f6dbeff2729dd8668660
LukusV2000 <lukusvmax@yahoo.ca>
2025-10-02 20:25:23 -0600

    modified:   app/main.c "blinking led done"

--- app/main.c
+++ app/main.c
@@ -7,21 +7,20 @@
 
 #define LED0_NODE DT_ALIAS(led0)
 
-static const struct gpio_dt_spec led0=GPIO_DT_SPEC_GET(LED0_NODE,gpios)
-
-};
+static const struct gpio_dt_spec led0=GPIO_DT_SPEC_GET(LED0_NODE,gpios);
 
 int main(void){
 if(!gpio_is_ready_dt(&led0)){
     return -1;
 }
 
-int ret=gpio_pin_configure_dt(&ledo,GPIO_OUTPUT_ACTIVE);
+int ret=gpio_pin_configure_dt(&led0,GPIO_OUTPUT_ACTIVE);
 if (ret <0){
     return ret;
 }
 while(1){
-
+gpio_pin_toggle_dt(&led0);
+k_msleep(1000);
     
 }
 
